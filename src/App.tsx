e31c1d8a0ca72b7d64eb1e9f9bb6972249efbddc
ChrisEuristic <eanair@kakao.com>
2023-08-28 20:49:48 +0900

    블로그 목표 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <main className={styles.App}>
+    <header>Chris's Devlog</header>
+    <article></article>
+    <footer>VS Code</footer>
+    <footer>TS</footer>
+    <footer>React</footer>
+    <footer>FabricJS</footer>
+    <footer>ChartJS</footer>
+    <footer>CesiumJS</footer>
+  </main>;
 }
 
 export default App;
