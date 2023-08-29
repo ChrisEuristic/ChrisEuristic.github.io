7d511770d26710087695d0c757d4b4e66a6dc151
ChrisEuristic <eanair@kakao.com>
2023-08-29 09:04:32 +0900

    markdown 적용 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import styles from './App.module.css';
+// utils
+import marked from "marked";
+
+// styles
+import styles from "./App.module.css";
+import { useRef, useState } from "react";
 
 function App() {
-  return <main className={styles.App}>
-    <header>Chris's Devlog</header>
-    <article></article>
-    <footer>VS Code</footer>
-    <footer>TS</footer>
-    <footer>React</footer>
-    <footer>FabricJS</footer>
-    <footer>ChartJS</footer>
-    <footer>CesiumJS</footer>
-  </main>;
+  const text = `#타이틀`;
+  const [result, setResult] = useState(marked.marked(text));
+
+  return (
+    <main className={styles.App}>
+      <header>Chris's Devlog</header>
+      <article>{result}</article>
+      <footer>VS Code</footer>
+      <footer>TS</footer>
+      <footer>React</footer>
+      <footer>FabricJS</footer>
+      <footer>ChartJS</footer>
+      <footer>CesiumJS</footer>
+    </main>
+  );
 }
 
 export default App;
